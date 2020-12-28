9167dde1d72d92e337d98beebaff9b0a24cebcb4
Xevion <xevioni@yandex.com>
2020-12-28 11:19:01 -0600

    repo init

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+nickname = input("Nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("Error! Disconnecting.")
+            client.close()
+            break
+
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
